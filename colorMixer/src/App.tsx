b901e2b8e59352e509ed39926635acbe01ff0e0f
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-04-10 20:17:33 +0530

    created buttons and initial state

--- colorMixer/src/App.tsx
+++ colorMixer/src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedValue,setSelectedValue] = useState("");
 
+  const handleRadioChange=(val:string)=>{
+    console.log(val)
+    setSelectedValue(val)
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h2>Create radio buttons here</h2>
+    <div id='container' style={{display:'flex',gap:'10px' }}>
+      <span style={{marginLeft:'75px'}}>
+        <input id='option1' type='radio' value={"red"} checked={selectedValue ==="red"} onChange={()=>handleRadioChange("red")}/>
+        <label htmlFor="option1">Red</label>
+      </span>
+      <span>
+        <input id='option2' type='radio' value={"green"} checked={selectedValue==="green"} onChange={()=> handleRadioChange("green")} />
+        <label htmlFor="option2">Green</label>
+      </span>
+      <span>
+        <input id='option3' type='radio' value={"blue"} checked={selectedValue==="blue"} onChange={()=> handleRadioChange("blue")}/>
+        <label htmlFor="option3">Blue</label>
+      </span>
+
+    </div>
+    <div id='colorBox' style={{width:'350px',height:'175px',backgroundColor:'black', marginTop:'20px'}}>
+
+</div>
     </>
   )
 }
