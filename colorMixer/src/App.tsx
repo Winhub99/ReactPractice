0bd1bdeab08c27430658a3513e56cd2b269f8141
Vinod Singh Mehta <vinod8mehat@gmail.com>
2024-04-11 14:50:44 +0530

    only with  combination of red

--- colorMixer/src/App.tsx
+++ colorMixer/src/App.tsx
@@ -1,37 +1,81 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
 
 function App() {
-  const [selectedValue,setSelectedValue] = useState("");
 
-  const handleRadioChange=(val:string)=>{
-    console.log(val)
-    setSelectedValue(val)
+  const [checkboxes, setCheckboxes] = useState({
+    checkbox1: false,
+    checkbox2: false,
+    checkbox3: false
+  })
+
+  const [finalColor,setFinalColor]= useState("")
+
+  const handleCheckboxes = (event: { target: { name: any; checked: any; }; }) => {
+    
+    const { name, checked } = event.target;
+    console.log("the name property is: ",name)
+    console.log("the checked property is: ",checked)
+    setCheckboxes((prev) =>({...prev,[name]:checked}))
+    // console.log(finalColor)
+
   }
+  useEffect(()=>{
 
-  const bgColor= (selectedValue==="red"?"red":selectedValue==="green"?"green":"blue");
+    function mixColors(col1:string,col2:string){
+
+      return `color-mix(in srgb,${col1},${col2})`
+    }
+    function setColor(){
+      if(checkboxes.checkbox1){
+        if(checkboxes.checkbox2){
+          const color=mixColors("red","green")
+          console.log("mixed color is:",color)
+          setFinalColor(color)
+        }else if(checkboxes.checkbox3){
+          const color=mixColors("red","blue")
+          setFinalColor(color)
+        }else{
+          setFinalColor("red")
+
+        }
+        
+      }else if(checkboxes.checkbox2){
+        setFinalColor("green")
+      }else if(checkboxes.checkbox3){
+        setFinalColor("blue")
+      }
+      
+      else{
+        setFinalColor("")
+      }
+    };
+    setColor();
+  },[checkboxes])
+
+  //const bgColor= (selectedValue==="red"?"red":selectedValue==="green"?"green":"blue");
   return (
     <>
-    <h2>Create radio buttons here</h2>
-    <div id='container' style={{display:'flex',gap:'10px' }}>
-      <span style={{marginLeft:'75px'}}>
-        <input id='option1' type='radio' value={"red"} checked={selectedValue ==="red"} onChange={()=>handleRadioChange("red")}/>
-        <label htmlFor="option1">Red</label>
-      </span>
-      <span>
-        <input id='option2' type='radio' value={"green"} checked={selectedValue==="green"} onChange={()=> handleRadioChange("green")} />
-        <label htmlFor="option2">Green</label>
-      </span>
-      <span>
-        <input id='option3' type='radio' value={"blue"} checked={selectedValue==="blue"} onChange={()=> handleRadioChange("blue")}/>
-        <label htmlFor="option3">Blue</label>
-      </span>
+      <h2>Create radio buttons here</h2>
+      <div id='container' style={{ display: 'flex', gap: '10px' }}>
+        <span style={{ marginLeft: '75px' }}>
+          <input id='option1' type='checkbox' name='checkbox1'  checked={checkboxes.checkbox1} onChange={handleCheckboxes} />
+          <label htmlFor="option1">Red</label>
+        </span>
+        <span>
+          <input id='option2' type='checkbox' name='checkbox2'  checked={checkboxes.checkbox2} onChange={handleCheckboxes} />
+          <label htmlFor="option2">Green</label>
+        </span>
+        <span>
+          <input id='option3' type='checkbox' name='checkbox3'  checked={checkboxes.checkbox3} onChange={handleCheckboxes} />
+          <label htmlFor="option3">Blue</label>
+        </span>
 
-    </div>
-    <div id='colorBox' style={{width:'350px',height:'175px',backgroundColor:bgColor, marginTop:'20px'}}>
+      </div>
+      <div id='colorBox' style={{ width: '350px', height: '175px', backgroundColor: finalColor, marginTop: '20px' }}  >
 
-</div>
+      </div>
     </>
   )
 }
